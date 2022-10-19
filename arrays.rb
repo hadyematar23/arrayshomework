5db420005178cf8c360a8aad693273d88f272345
Hady Matar <hadyematar@gmail.com>
2022-10-19 11:09:54 -0600

    Call four methods

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,17 @@
 beaches = ["punta", "zicatela", "bacocho", "agua blanca"]
 integers = [23, 34, 54, 86]
-flaots = [0.3, 3.14, 8.6, 9.77]
+floats = [0.3, 3.14, 8.6, 9.77]
 booleans = [true, false, false, true]
+
+
+# If I Call the .pop method on this array, it will return only the last element in the array
+puts beaches.pop
+
+# If I call the .shift method on this array, it will return only the first element in the array 
+puts integers.shift
+
+# iF I call the .push method on this array, it will append the value I provide to the end of the array. 
+puts floats.push(14.25)
+
+# If I call the unshift method, it will append the element I provide (in this case, the Boolean false) the beginning of the array
+puts booleans.unshift(false)
